07fd9cf934d3721baa1757936b0e10412c3a1236
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-06-28 13:37:47 +0530

    add new program

--- Recursion.java
+++ Recursion.java
@@ -1,19 +1,21 @@
-public class Recursion{
-    public static void printFib(int a,int b,int n){
-        if(n ==0){
-            return;
+public class Recursion(
+    public static void calcPower(int x,int n){
+        if(n==0){
+            return 1;
 
         }
-        int c=a+b;
-        System.out.println(c);
-        printFib(b,c,n-1);
-    }
-    public static void main(String args[]){
-        int a =0,b=1;
-        System.out.println(a);
-        System.out.println(b);
-        int n=7;
-        printFib(a,b,n-2);
+        if(x==0)(
+            return 0;
+        )
+        int xpownm1=calcPower(x,n-1);
+        int xpown=x*xpownm1;
+        return xpown;
 
     }
-}
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 3;
+        int result = calcPower(x, n);
+        System.out.println("Result: " + result);
+    }
+)
