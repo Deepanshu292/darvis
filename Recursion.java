faf2d7b659f6e16815aae62d9c6d2873a478a358
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-06-28 18:19:38 +0530

    add new program to a new file

--- Recursion.java
+++ Recursion.java
@@ -1,21 +1,19 @@
-public class Recursion(
-    public static void calcPower(int x,int n){
-        if(n==0){
-            return 1;
-
-        }
-        if(x==0)(
-            return 0;
-        )
-        int xpownm1=calcPower(x,n-1);
-        int xpown=x*xpownm1;
-        return xpown;
+public class Recursion{
+    if(n==1){
+        System.out.println("Move disk 1 from " + src + " to " + dest);
+        return;
+    }
+    // Move n-1 disks from source to helper
+    public static void towerfhanoi(int n,String src ,String helper , Stringdest){
+        towerofhanoi(n-1 ,src ,dest,helper);
+        
+        System.out.println("Move disk " + n + " from " + src + " to " + dest);
+        towerofhanoi(n-1 ,helper ,src,dest);
+        
 
     }
-    public static void main(String[] args) {
-        int x = 2;
-        int n = 3;
-        int result = calcPower(x, n);
-        System.out.println("Result: " + result);
+    public static void main(String args[]){
+        int n=1;
+        towerofhanoi(n,"S", "H", "D");
     }
-)
+}
