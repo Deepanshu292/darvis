8fcfce3b3ef00fa0d9d6443486dff3fb2548d4bd
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-06-29 08:58:09 +0530

    add multiple programs

--- Recursion.java
+++ Recursion.java
@@ -1,19 +1,27 @@
 public class Recursion{
-    if(n==1){
-        System.out.println("Move disk 1 from " + src + " to " + dest);
-        return;
-    }
-    // Move n-1 disks from source to helper
-    public static void towerfhanoi(int n,String src ,String helper , Stringdest){
-        towerofhanoi(n-1 ,src ,dest,helper);
-        
-        System.out.println("Move disk " + n + " from " + src + " to " + dest);
-        towerofhanoi(n-1 ,helper ,src,dest);
-        
+    public static int first =-1;
+    public static int last =-2;
+    public static void findOccurance(String str,int idx,char element){
+        if(idx ==str.length()){
+            System.out.println("First Occurance of " + element + " is at index: " + first);
+            System.out.println("Last Occurance of " + element + " is at index: " + last);
+            return;
+        }   
+        char currChar=str.charAt(idx);
+        if(currChar ==element){
+            if(first==-1){
+                first=idx;
+                
+            }else{
+                last=idx;
+            }
+        }
+        findOccurance(str,idx+1,element);
+
 
     }
     public static void main(String args[]){
-        int n=1;
-        towerofhanoi(n,"S", "H", "D");
+        String str = "abaacdaefaah";
+        findOccurance(str,0,'a');
     }
 }
